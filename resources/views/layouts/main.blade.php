755a5e5cbedc307ea46d21e71810579087c344d1
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-06-02 17:50:38 +0300

    edit adminlte*2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -198,9 +198,6 @@
     <footer class="main-footer">
         <strong>Copyright &copy; 2024 -{{ now()->year }} <a href="{{route('main.index')}}">Laravel Store</a>.</strong>
         Все права защищены.
-        <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.2.0
-        </div>
     </footer>
 
     <!-- Control Sidebar -->
